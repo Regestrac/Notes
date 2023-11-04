6a1631c31f9a9a7f38feeb81c05a23ca0d3258aa
Sojo C Johny <sojocjohny37@gmail.com>
2023-11-04 13:44:22 +0530

    Created NotesRepository & NotesAdaptor & created function to generate random color for notes background

---
+++ app/src/main/java/com/regestrac/notes/Adapter/NotesAdapter.kt
@@ -0,0 +1,60 @@
+package com.regestrac.notes.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.regestrac.notes.Modals.Note
+import com.regestrac.notes.R
+import kotlin.random.Random
+
+class NotesAdapter(private val context: Context): RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+
+    private val NotesList: ArrayList<Note>()
+    private val FullList: ArrayList<Note>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return NotesList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = NotesList[position]
+        holder.title.text = currentNote.title
+        holder.title.isSelected = true
+        holder.note_tv.text = currentNote.note
+        holder.date.text = currentNote.date
+        holder.date.isSelected = true
+        holder.notes_layout.setCardBackgroundColor(holder.itemView.resources.getColor(randomColor(), null ))
+    }
+
+    private fun randomColor(): Int {
+        val list = ArrayList<Int>()
+        list.add(R.color.NoteColor1)
+        list.add(R.color.NoteColor2)
+        list.add(R.color.NoteColor3)
+        list.add(R.color.NoteColor4)
+        list.add(R.color.NoteColor5)
+        list.add(R.color.NoteColor6)
+        list.add(R.color.NoteColor7)
+
+        val seed = System.currentTimeMillis().toInt()
+        val randomIndex = Random(seed).nextInt(list.size)
+        return list[randomIndex]
+    }
+
+    inner class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val notes_layout = itemView.findViewById<CardView>(R.id.card_layout)
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val note_tv = itemView.findViewById<TextView>(R.id.tv_note)
+        val date = itemView.findViewById<TextView>(R.id.tv_date)
+    }
+}
