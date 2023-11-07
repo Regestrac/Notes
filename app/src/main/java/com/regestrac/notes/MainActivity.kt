437123c26b6ac610f406d471d25f9c5d5310a9f5
Sojo C Johny <sojocjohny37@gmail.com>
2023-11-07 21:25:12 +0530

    Added NotesViewModal and added option for initializing UI in the mainActivity file

--- app/src/main/java/com/regestrac/notes/MainActivity.kt
+++ app/src/main/java/com/regestrac/notes/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.regestrac.notes
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.SearchView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModel
+import com.regestrac.notes.Modals.Note
+import com.regestrac.notes.Modals.NoteViewModal
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.regestrac.notes.Adapter.NotesAdapter
+import com.regestrac.notes.Database.NoteDatabase
+import com.regestrac.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: NoteDatabase
+    lateinit var viewModal: NoteViewModal
+    lateinit var adapter: NotesAdapter
+    lateinit var selectedNote: Note
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // For initialising the UI
+        initUI()
+
+        viewModal = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModal::class.java)
+
+        viewModal.allnotes.observe(this) {list ->
+            list?.let {
+                adapter.updateList(list)
+            }
+        }
+
+        database = NoteDatabase.getDatabase(this)
+
+    }
+
+    private fun initUI() {
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter = NotesAdapter(this, this)
+        binding.recyclerView.adapter = adapter
+
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val note = result.data?.getSerializableExtra("note") as? Note
+                if (note != null) {
+                    viewModal.insertNote(note)
+                }
+            }
+        }
+
+        binding.fbAddNote.setOnClickListener() {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            getContent.launch(intent)
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    adapter.filterList(newText)
+                }
+                    return true
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/regestrac/notes/Modals/NoteViewModal.kt
@@ -0,0 +1,34 @@
+package com.regestrac.notes.Modals
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.regestrac.notes.Database.NoteDatabase
+import com.regestrac.notes.Database.NotesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModal(application: Application): AndroidViewModel(application) {
+    private val repository: NotesRepository
+
+    val allnotes: LiveData<List<Note>>
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        repository = NotesRepository(dao)
+        allnotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun insertNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+}
