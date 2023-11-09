b557c44107eb09f20f5e71808f432b9ea0502b17
Sojo C Johny <sojocjohny37@gmail.com>
2023-11-09 21:19:12 +0530

    Added the functionality for saving the note and back arrow press

--- app/src/main/java/com/regestrac/notes/AddNoteActivity.kt
+++ app/src/main/java/com/regestrac/notes/AddNoteActivity.kt
@@ -1,11 +1,63 @@
 package com.regestrac.notes
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.regestrac.notes.Modals.Note
+import com.regestrac.notes.databinding.ActivityAddNoteBinding
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class AddNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var note: Note
+    private lateinit var oldNote: Note
+    private var isUpdate = false
+
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+            oldNote = intent.getSerializableExtra("current_note") as Note
+            binding.etTitle.setText(oldNote.title)
+            binding.etNote.setText(oldNote.note)
+            isUpdate = true
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        binding.imgCheck.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val noteDescription = binding.etNote.text.toString()
+
+            if (title.isNotEmpty() || noteDescription.isNotEmpty()) {
+                val formatter = SimpleDateFormat("EEE, d MMM yyyy HH:mm a")
+
+                if (isUpdate) {
+                    note = Note(oldNote.id, title, noteDescription, formatter.format(Date()))
+                } else {
+                    note = Note(null, title, noteDescription, formatter.format(Date()))
+                }
+
+                val intent = Intent()
+                intent.putExtra("note", note)
+                setResult(Activity.RESULT_OK, intent)
+            } else {
+                Toast.makeText(this@AddNoteActivity, "Please enter some data", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+        }
+
+        binding.imgBackArrow.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
